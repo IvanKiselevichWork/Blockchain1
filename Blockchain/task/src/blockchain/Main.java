0f2df15316b18f29bfd4c29272b791e67f103ee2
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-09-16 19:29:33 +0300

    Complete Stage 3

--- Blockchain/task/src/blockchain/Main.java
+++ Blockchain/task/src/blockchain/Main.java
@@ -5,7 +5,6 @@ import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 class Block {
@@ -41,18 +40,20 @@ class Block {
         this.magicNumber = magicNumber;
     }
 
-    public int getGeneratingTimeInSeconds() {
-        return generatingTimeInSeconds;
-    }
-
     public void setGeneratingTimeInSeconds(int generatingTimeInSeconds) {
         this.generatingTimeInSeconds = generatingTimeInSeconds;
+    }
+
+    public int getGeneratingTimeInSeconds() {
+        return generatingTimeInSeconds;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Block:\n");
+        sb.append("Created by miner # " + ThreadLocalRandom.current().nextInt(1,10));
+        sb.append("\n");
         sb.append("Id: ");
         sb.append(id);
         sb.append("\n");
@@ -68,66 +69,75 @@ class Block {
         sb.append("Hash of the block: \n");
         sb.append(StringUtil.applySha256(this));
         sb.append("\n");
-        sb.append("lock was generating for ");
+        sb.append("block was generating for ");
         sb.append(generatingTimeInSeconds);
         sb.append(" seconds");
-        sb.append("\n");
 
         return sb.toString();
     }
 }
 
 class Blockchain {
-    private List<Block> blockchain = new ArrayList<>();
-    private int zerosCountInStart;
-
-    public Blockchain(int zerosCountInStart) {
-        this.zerosCountInStart = zerosCountInStart;
-    }
+    private static final int MIN_ZERO_COUNT = 0;
+    private static final int MAX_ZERO_COUNT = 30;
+    private static final int MIN_GENERATING_TIME_IN_SECONDS = 5;
+    private static final int MAX_GENERATING_TIME_IN_SECONDS = 10;
+    private final List<Block> blocks = new ArrayList<>();
+    private int nextBlockZeroCount = 0;
 
     public Block getFirstBlock() {
-        return blockchain.get(0);
+        return blocks.get(0);
     }
 
     public List<Block> getBlockchain() {
-        return new ArrayList<>(blockchain);
+        return new ArrayList<>(blocks);
     }
 
-    public Block addBlock() {
+    public Block generateBlock(int zeroCount) {
         long startTime = System.currentTimeMillis();
         String prevHash = "0";
-        if (!blockchain.isEmpty()) {
-            prevHash = StringUtil.applySha256(blockchain.get(blockchain.size() - 1));
+        if (!blocks.isEmpty()) {
+            prevHash = StringUtil.applySha256(blocks.get(blocks.size() - 1));
         }
-        String zerosStandard = new String(new char[zerosCountInStart]).replaceAll("\0", "0");
+        String zerosStandard = new String(new char[zeroCount]).replaceAll("\0", "0");
 
         String zerosPart;
         int magicNumber;
-        Block block = new Block((long) blockchain.size(), new Date().getTime(), prevHash);
+        Block block = new Block((long) blocks.size(), new Date().getTime(), prevHash);
         do {
             magicNumber = ThreadLocalRandom.current().nextInt();
             block.setMagicNumber(magicNumber);
-            zerosPart = StringUtil.applySha256(block).substring(0, zerosCountInStart);
+            zerosPart = StringUtil.applySha256(block).substring(0, zeroCount);
         } while (!zerosStandard.equals(zerosPart));
         long endTime = System.currentTimeMillis();
         int duration = (int) ((endTime - startTime) / 1000);
         block.setGeneratingTimeInSeconds(duration);
-        blockchain.add(block);
         return block;
     }
 
+    public int addBlock(Block block) {
+        //todo check if block valid
+        blocks.add(block);
+        if (block.getGeneratingTimeInSeconds() < MIN_GENERATING_TIME_IN_SECONDS && nextBlockZeroCount < MAX_ZERO_COUNT) {
+            nextBlockZeroCount++;
+        } else if (block.getGeneratingTimeInSeconds() > MAX_GENERATING_TIME_IN_SECONDS && nextBlockZeroCount > MIN_ZERO_COUNT) {
+            nextBlockZeroCount--;
+        }
+        return nextBlockZeroCount;
+    }
+
     public boolean isBlockchainValid() {
-        if (blockchain.isEmpty()) {
+        if (blocks.isEmpty()) {
             return true;
         }
-        if (!blockchain.get(0).getPreviousBlockHash().equals("0")) {
+        if (!blocks.get(0).getPreviousBlockHash().equals("0")) {
             return false;
         }
-        if (blockchain.size() == 1) {
+        if (blocks.size() == 1) {
             return true;
         }
-        for (int i = 0; i < blockchain.size() - 1; i++) {
-            if (!StringUtil.applySha256(blockchain.get(i)).equals(blockchain.get(i + 1).getPreviousBlockHash())) {
+        for (int i = 0; i < blocks.size() - 1; i++) {
+            if (!StringUtil.applySha256(blocks.get(i)).equals(blocks.get(i + 1).getPreviousBlockHash())) {
                 return false;
             }
         }
@@ -137,7 +147,7 @@ class Blockchain {
     @Override
     public String toString() {
         return "Blockchain{" +
-                "blockchain=" + blockchain +
+                "blockchain=" + blocks +
                 '}';
     }
 }
@@ -146,30 +156,6 @@ class StringUtil {
     /* Applies Sha256 to a string and returns a hash. */
     public static String applySha256(String input) {
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            /* Applies sha256 to our input */
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuilder hexString = new StringBuilder();
-            for (byte elem : hash) {
-                String hex = Integer.toHexString(0xff & elem);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            return hexString.toString();
-        } catch (Exception e) {
-            throw new RuntimeException("Hashing error", e);
-        }
-    }
-
-    /* Applies Sha256 to a Block and returns a hash. */
-    public static String applySha256(Block block) {
-        try {
-            StringBuilder sb = new StringBuilder();
-            sb.append(block.getId());
-            sb.append(block.getTimestamp());
-            sb.append(block.getPreviousBlockHash());
-            sb.append(block.getMagicNumber());
-            String input = sb.toString();
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             /* Applies sha256 to our input */
             byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
@@ -184,19 +170,39 @@ class StringUtil {
             throw new RuntimeException("Hashing error", e);
         }
     }
+
+    /* Applies Sha256 to a Block and returns a hash. */
+    public static String applySha256(Block block) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(block.getId());
+        sb.append(block.getTimestamp());
+        sb.append(block.getPreviousBlockHash());
+        sb.append(block.getMagicNumber());
+        String input = sb.toString();
+        return applySha256(input);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Enter how many zeros the hash must start with: ");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-
-        Blockchain blockchain = new Blockchain(n);
+        Blockchain blockchain = new Blockchain();
+        int zeroCount = 0;
+        int previousZeroCount = 0;
+        Block block;
         for (int i = 0; i < 5; i++) {
-            System.out.println(blockchain.addBlock());
+            block = blockchain.generateBlock(zeroCount);
+            previousZeroCount = zeroCount;
+            zeroCount = blockchain.addBlock(block);
+            System.out.println(block);
+            if (previousZeroCount == zeroCount) {
+                System.out.println("N stays the same");
+            } else if (zeroCount > previousZeroCount) {
+                System.out.println("N was increased to " + zeroCount);
+            } else {
+                System.out.println("N was decreased by " + (previousZeroCount - zeroCount));
+            }
+            System.out.println();
         }
     }
 }
-

769a911e452cd8c0d643bf6503ab14957fe7e857
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-10-01 00:12:55 +0300

    Add Set average solution

---
+++ Problems/Set average/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+class Main {
+
+    /**
+     * Returns the average value of numbers from a Set
+     * that is accumulated from the input stream.
+     *
+     * @param numbers the input stream of Integer elements
+     * @return average value of a Set of numbers
+     */
+    public static Double avgOnSet(Stream<Integer> numbers) {
+        return numbers.distinct().mapToInt(value -> value).average().orElse(0);
+    }
+
+    // Don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stream<Integer> integerStream = Arrays.stream(scanner.nextLine()
+                .split("\\s+")).map(Integer::parseInt);
+        System.out.println(avgOnSet(integerStream));
+    }
+}

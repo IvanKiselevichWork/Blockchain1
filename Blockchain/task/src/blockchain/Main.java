e28f19f84ed6512d084c2cf9021d53a57b30393b
Ivan Kiselevich <ivankiselevichwork@gmail.com>
2020-10-01 18:35:23 +0300

    Complete Stage 5/6: Matters of security

--- Blockchain/task/src/blockchain/Main.java
+++ Blockchain/task/src/blockchain/Main.java
@@ -1,11 +1,138 @@
 package blockchain;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
+import java.security.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+
+class GenerateKeys {
+
+    private final KeyPairGenerator keyGen;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public GenerateKeys(int keylength) throws NoSuchAlgorithmException {
+        this.keyGen = KeyPairGenerator.getInstance("RSA");
+        this.keyGen.initialize(keylength);
+    }
+
+    public void createKeys() {
+        KeyPair pair = this.keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public PrivateKey getPrivateKey() {
+        return this.privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return this.publicKey;
+    }
+
+    public void writeToFile(String path, byte[] key) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(key);
+        fos.flush();
+        fos.close();
+    }
+}
+
+class MessageUtil {
+
+    private static long id = 0;
+
+    public static String getId() {
+        return String.valueOf(id++);
+    }
+
+    public static Message generateMessage(String data) {
+        try {
+            GenerateKeys generateKeys = new GenerateKeys(1024);
+            generateKeys.createKeys();
+            String identifier = getId();
+            PublicKey publicKey = generateKeys.getPublicKey();
+            PrivateKey privateKey = generateKeys.getPrivateKey();
+            String sign = getMessageSign(data, identifier, privateKey);
+            return new Message(data, sign, identifier, publicKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getMessageSign(String data, String identifier, PrivateKey privateKey) {
+        try {
+            Signature rsa = Signature.getInstance("SHA1withRSA");
+            rsa.initSign(privateKey);
+            rsa.update((data + identifier).getBytes());
+            return new String(rsa.sign());
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean isMessageValid(Message message) {
+        try {
+            Signature sig = Signature.getInstance("SHA1withRSA");
+            sig.initVerify(message.getPublicKey());
+            sig.update((message.getData() + message.getIdentifier()).getBytes());
+            return sig.verify(message.getSign().getBytes());
+        } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
+class Message {
+    private final String data;
+    private final String sign;
+    private final String identifier;
+    private final PublicKey publicKey;
+
+    public Message(String data, String sign, String identifier, PublicKey publicKey) {
+        this.data = data;
+        this.sign = sign;
+        this.identifier = identifier;
+        this.publicKey = publicKey;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "data='" + data + '\'' +
+                ", sign='" + sign + '\'' +
+                ", identifier='" + identifier + '\'' +
+                ", publicKey=" + publicKey +
+                '}';
+    }
+}
 
 class Block {
     private final Long id;
@@ -13,7 +140,7 @@ class Block {
     private final String previousBlockHash;
     private int magicNumber;
     private int generatingTimeInSeconds;
-    private String message;
+    private List<Message> messages;
 
     public Block(Long id, Long timestamp, String previousBlockHash) {
         this.id = id;
@@ -49,19 +176,23 @@ class Block {
         return generatingTimeInSeconds;
     }
 
-    public String getMessage() {
-        return message;
+    public List<Message> getMessages() {
+        if (messages == null) {
+            return null;
+        } else {
+            return List.copyOf(messages);
+        }
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setMessage(List<Message> messages) {
+        this.messages = new ArrayList<>(messages);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Block:\n");
-        sb.append("Created by miner # " + ThreadLocalRandom.current().nextInt(1,10));
+        sb.append("Created by miner # " + ThreadLocalRandom.current().nextInt(1, 10));
         sb.append("\n");
         sb.append("Id: ");
         sb.append(id);
@@ -78,8 +209,12 @@ class Block {
         sb.append("Hash of the block: \n");
         sb.append(StringUtil.applySha256(this));
         sb.append("\n");
-        sb.append("Block data: ");
-        sb.append(message == null ? "no messages" : message);
+        sb.append("Block data: \n");
+        if (messages == null) {
+            sb.append("no messages");
+        } else {
+            messages.forEach(message -> sb.append(message.getData()));
+        }
         sb.append("\n");
         sb.append("Block was generating for ");
         sb.append(generatingTimeInSeconds);
@@ -152,6 +287,14 @@ class Blockchain {
             if (!StringUtil.applySha256(blocks.get(i)).equals(blocks.get(i + 1).getPreviousBlockHash())) {
                 return false;
             }
+            List<Message> messages = blocks.get(i + 1).getMessages();
+            if (messages != null) {
+                for(Message message : messages) {
+                    if (!MessageUtil.isMessageValid(message)) {
+                        return false;
+                    }
+                }
+            }
         }
         return true;
     }
@@ -190,7 +333,9 @@ class StringUtil {
         sb.append(block.getTimestamp());
         sb.append(block.getPreviousBlockHash());
         sb.append(block.getMagicNumber());
-        sb.append(block.getMessage());
+        if (block.getMessages() != null) {
+            sb.append(block.getMessages().toString());
+        }
         String input = sb.toString();
         return applySha256(input);
     }
@@ -205,8 +350,10 @@ public class Main {
         Block block;
         for (int i = 0; i < 5; i++) {
             block = blockchain.generateBlock(zeroCount);
+            Message message = null;
             if (i != 0) {
-                block.setMessage("message" + i);
+                message = MessageUtil.generateMessage("message" + i);
+                block.setMessage(List.of(message));
             }
             previousZeroCount = zeroCount;
             zeroCount = blockchain.addBlock(block);
